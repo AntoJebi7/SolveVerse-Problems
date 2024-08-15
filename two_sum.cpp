a616a27df92dece6fa5cc9f91f90f2c5de6c845a
AntoJebi7 <dantojebi@gmail.com>
2024-08-15 19:33:37 +0530

    leetcode day 1

---
+++ two_sum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> twosum(vector<int>& v1,int target ){
+    for (int i = 0; i < v1.size(); i++)
+    {
+        for (int j = i+1; j < v1.size(); j++)
+        {
+           if (v1[i]+v1[j]==target)
+           {
+            return {i,j};
+           }
+           
+        }
+        
+        
+    }
+    return {};
+}
+int main(){
+    vector<int> v1= {23,12,354,56,343};
+    int target;
+    cout<<"Enter Target to Search : "<<endl;
+    cin>>target;
+    twosum( v1,target);
+    
+    return 0;
+}
