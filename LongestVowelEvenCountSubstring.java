9ff56734e875a92f8541b0beba51941b592cf92d
Anto Jebikshan <dantojebi@gmail.com>
2024-09-15 22:09:07 +0530

    🔥  Streak day 4

--- LongestVowelEvenCountSubstring.java
+++ LongestVowelEvenCountSubstring.java
@@ -55,3 +55,31 @@ public class LongestVowelEvenCountSubstring {
         System.out.println("The length of the longest substring is: " + solution.findTheLongestSubstring(s)); // Output should be 13
     }
 }
+
+
+/*
+ *  tutorial suggestion : https://youtu.be/BI4h1IwfADs?si=Q5XrwJF21eaerocW
+ * 
+ * 1371. Find the Longest Substring Containing Vowels in Even Counts
+Solved
+Medium
+Given the string s, return the size of the longest substring containing each vowel an even number of times. That is, 'a', 'e', 'i', 'o', and 'u' must appear an even number of times.
+
+ 
+
+Example 1:
+
+Input: s = "eleetminicoworoep"
+Output: 13
+Explanation: The longest substring is "leetminicowor" which contains two each of the vowels: e, i and o and zero of the vowels: a and u.
+Example 2:
+
+Input: s = "leetcodeisgreat"
+Output: 5
+Explanation: The longest substring is "leetc" which contains two e's.
+Example 3:
+
+Input: s = "bcbcbc"
+Output: 6
+Explanation: In this case, the given string "bcbcbc" is the longest because all vowels: a, e, i, o and u appear zero times.
+ */
